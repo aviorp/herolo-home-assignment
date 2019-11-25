0cc55ea22af59cdc1c612ad59fd71d6608f3b976
Avior Aharon <avior767@gmail.com>
2019-11-26 00:55:13 +0200

    Added Css

--- src/components/DaysCard/DaysCard.js
+++ src/components/DaysCard/DaysCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const DaysCard = props => {
   return (
     <div>
-      <h1>{props.city}</h1>
+      <h1>{props.day}</h1>
       <h3>{props.degs}</h3>
       <img src={props.img} />
     </div>
--- src/components/MainCard/MainCard.js
+++ src/components/MainCard/MainCard.js
@@ -3,20 +3,35 @@ import { Row, Col, Container } from "react-bootstrap";
 import "./MainCard.css";
 const MainCard = props => {
   return (
-    <>
-      <div className="wa-main-card">
-        <Row className="wa-row">
-          <Col md={2}>
-            <img src={"https://picsum.photos/150/150"} />
-            <h3>props.img</h3>
-          </Col>
-          <Col className="wa-right" sm={10}>
-            <h5>Add to Favorite big icon</h5>
-          </Col>
-        </Row>
-        <hr />
-      </div>
-    </>
+    <Container fluid={true} className="wa-main-card">
+      <Row className="wa-row">
+        <Col md={1}>
+          <img src={"https://picsum.photos/150/150"} />
+          <h3>props.img</h3>
+        </Col>
+        <Col md={2} className="wa-header-details">
+          <h4>props.city</h4>
+          <br />
+          <p>props.deg</p>
+        </Col>
+        <Col className="wa-right" md={2}>
+          <h5>x</h5>
+        </Col>
+      </Row>
+      <hr />
+      <Row className="wa-row">
+        <Col>
+          <h1>Very Interesting Content...</h1>
+          <h3>props.weatherStatus</h3>
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col sm={12}>
+          <footer>Cards Goes Here</footer>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
