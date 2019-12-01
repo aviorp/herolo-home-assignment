d4a7a81a94cc43fb5ec80fb37092cb4fd255438a
Avior Aharon <avior767@gmail.com>
2019-12-01 17:31:34 +0200

    Updates

---
+++ precache-manifest.a0ed57e02d617fe4504e991e4b90e8a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e801726c34eedaed3aebced84da38e82",
+    "url": "/avior-aharon-24-11-19/index.html"
+  },
+  {
+    "revision": "cbc9812e3b4ad7c4322e",
+    "url": "/avior-aharon-24-11-19/static/css/2.b7f43d44.chunk.css"
+  },
+  {
+    "revision": "7693bff6c53fd312a960",
+    "url": "/avior-aharon-24-11-19/static/css/main.4830ad3e.chunk.css"
+  },
+  {
+    "revision": "cbc9812e3b4ad7c4322e",
+    "url": "/avior-aharon-24-11-19/static/js/2.cc42eaba.chunk.js"
+  },
+  {
+    "revision": "7693bff6c53fd312a960",
+    "url": "/avior-aharon-24-11-19/static/js/main.c8bcfd61.chunk.js"
+  },
+  {
+    "revision": "b7097d1a84c840fa1a6d",
+    "url": "/avior-aharon-24-11-19/static/js/runtime-main.7e734ec9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/avior-aharon-24-11-19/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/avior-aharon-24-11-19/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/avior-aharon-24-11-19/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/avior-aharon-24-11-19/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/avior-aharon-24-11-19/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
